83a34d02f7fd78c18b5509cea9e065c56a2e54c9
kjy10202157-lgtm <kjy10202157@gmail.com>
2026-09-03 08:59:36 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -6,8 +6,8 @@ const ASSETS = [
   "./game.js",
   "./manifest.webmanifest",
   "./privacy.html",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
