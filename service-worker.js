2526486f75263499eef649c92c1145031d481fd6
kjy10202157-lgtm <kjy10202157@gmail.com>
2026-09-05 16:03:36 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,12 @@
-const CACHE = "neon-clash-v18";
+const CACHE = "neon-clash-v22";
 const ASSETS = [
   "./",
   "./index.html",
   "./style.css",
   "./game.js",
+  "./online.html",
+  "./online.css",
+  "./online.js",
   "./manifest.webmanifest",
   "./privacy.html",
   "./icon-192.png",
