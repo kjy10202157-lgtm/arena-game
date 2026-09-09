682685bbc885439a28d0a38d6ada6bc3c281fc12
kjy10202157-lgtm <kjy10202157@gmail.com>
2026-09-09 05:48:02 +0000

    Connect online game to Cloudflare server

--- online-config.js
+++ online-config.js
@@ -1,3 +1,1 @@
-// Cloudflare Worker를 배포한 뒤 workers.dev 주소를 따옴표 안에 넣으세요.
-// 예: window.NEON_CLASH_SERVER = "https://neon-clash-server.계정명.workers.dev";
-window.NEON_CLASH_SERVER = "";
+window.NEON_CLASH_SERVER = "https://neon-clash-server.kjy10202157.workers.dev";
